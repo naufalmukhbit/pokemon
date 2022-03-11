59004d4c544c2cbd1ab2c03f7dc6281f23111b01
Muhammad Naufal Mukhbit Amrullah <muhammad.naufal@ai.astra.co.id>
2022-03-11 08:11:13 +0700

    Add pokemon logo to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,34 +4,45 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   const headerContainerStyle = css({
+    background: "white",
     position: "sticky",
     top: 0,
-    background: "white",
-  })
-
+  });
+  
   const headerStyle = css({
+    height: 45,
     display: "flex",
     flexDirection: "row",
     justifyContent: "space-between",
-    padding: "1rem"
+    paddingLeft: "0.5rem",
+    paddingRight: "1rem",
+    alignItems: "center",
+  });
+  
+  const headerLogoStyle = css({
+    height: 30,
   })
-
+  
   const linkStyle = css({
     textDecoration: "none",
-    color: "black"
-  })
+    color: "black",
+  });
   return (
     <div css={headerContainerStyle}>
       <div css={headerStyle}>
-        <Link to="/" css={linkStyle}>
-          <span>Pokemon</span>
+        <Link to="/">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/9/98/International_Pok%C3%A9mon_logo.svg"
+            alt="Pokemon logo"
+            css={headerLogoStyle}
+          />
         </Link>
         <Link to="/my-pokemon" css={linkStyle}>
           <span>My Pokémon</span>
         </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Header;
